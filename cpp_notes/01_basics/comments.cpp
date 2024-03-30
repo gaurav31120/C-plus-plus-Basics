ea2d7628e60036d66111386e29a45accd6a926c1
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-30 09:57:36 +0530

    comments

---
+++ cpp_notes/01_basics/comments.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Single line comment ----- put // before the line you want to comment
+    // int a=7;
+
+    // This example uses a single-line comment at the end of a line of code:
+    int b = 4; // single line comment at the end  of the line.
+
+    // Multi line comments ------
+    //  Multi-line comments start with /* and ends with */.
+    //  Any text between /* and */ will be ignored by the compiler:
+    return 0;
+}
