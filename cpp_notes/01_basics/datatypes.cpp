e11cc689f8cc1fa98ba3271c4d7ee5c371c894e3
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-30 09:51:05 +0530

    variables and datatypes

---
+++ cpp_notes/01_basics/datatypes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // variable declaration //This is a comment
+
+    // integer -- takes 4 byte = 32 bits
+    int a = 10, b = 20, c = 30, d, e = 11;
+
+    // character -- takes 1 byte = 8 bits
+    char c = 'a';
+
+    // float -- takes 4 byte = 32 bits
+    float f = 2.31;
+
+    // double -- takes 8 byte = 64 bits
+    double d = 3.121;
+
+    // boolean -- takes 1 byte = 2 bits
+    bool b = true; // 1
+    bool b1 = 1;   // 1
+
+    bool e = false; // 0
+    bool e1 = 0;    // 0
+
+    return 0;
+}
