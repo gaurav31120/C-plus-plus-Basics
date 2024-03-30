4bf7c606558c897cfdae5a27bb79c7d8ce2c90f5
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-30 10:07:11 +0530

    cout and endl, \n

---
+++ cpp_notes/01_basics/printingOutput.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "My name is Gaurav"; // My name is Gaurav
+
+    cout << "I am from India";
+
+    // endl ---- this will take you to the next line.
+    // /n ---- this will also take you to the next line but the syntax writing is different than endl
+    cout << "This is first line" << endl;
+
+    cout << "This is third line \n"; // syntax of \n is inside the double quotes(" \n")
+
+    cout << "This is fourth line";
+
+    return 0;
+}
