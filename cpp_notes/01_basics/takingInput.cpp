bb89a21dc5e3ae29d98a59b3d83d35032b99c3f6
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-30 10:14:11 +0530

    cin>> input

---
+++ cpp_notes/01_basics/takingInput.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 20;
+    // cin>> is used to take input from the system
+
+    // << -- insertion operator
+    // >> -- extraction operator
+
+    // use space or enter to give multiple inputs
+
+    cin >> a;      // 5
+    cin >> b;      // 6
+    cout << a + b; // 11
+
+    return 0;
+}
