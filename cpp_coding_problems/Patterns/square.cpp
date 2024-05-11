fee644e1357a70f424e78883d52579cb52d2b45d
Gaurav Kumar <gaurav31120@gmail.com>
2024-05-11 18:27:23 +0530

    square pattern

---
+++ cpp_coding_problems/Patterns/square.cpp
@@ -0,0 +1,20 @@
+// Print square pattern
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of rows of a square: " << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
