9a115510751c61bf7f7fbb34c16772d8f0a70b25
Gaurav Kumar <gaurav31120@gmail.com>
2024-05-11 19:39:37 +0530

    half pyramid

---
+++ cpp_coding_problems/Patterns/halfPyramid.cpp
@@ -0,0 +1,26 @@
+// Print half pyramid pattern
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter size of pyramid: " << endl;
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// output will be like this
+//  *
+//  * *
+//  * * *
+//  * * * *
+//  * * * * *
